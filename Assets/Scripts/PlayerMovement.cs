9a344d94698e78f44cb1addfb6cdbfa2dec8fa69
thatjamesemo <jamestravisje@gmail.com>
2023-03-30 15:17:26 +0100

    Added Modified movement
    Changelog:
     - Modified mouse visibility and confinement
     - Modified mouse sensitivity variable.
     - Modified the movement to become relative force rather than velocity.
     - Added a respawn element. Can be modified to become spawn points later on.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    [SerializeField] float movementModifier = 5f;
+    [SerializeField] float movementModifier = 0.1f;
     [SerializeField] float jumpModifier = 6f;
     Rigidbody rb;
     Transform transform;
 
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask ground;
+    bool cursorVisibility = false;
+    [SerializeField] float mouseSensitivityX = 2f;
 
 
 
@@ -25,17 +27,25 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float hi = Input.GetAxis("Horizontal"); // Horizontal Momvement Variable
-        float vi = Input.GetAxis("Vertical"); // Vertical Movement Variable
+        float hi = Input.GetAxisRaw("Horizontal"); // Horizontal Momvement Variable
+        float vi = Input.GetAxisRaw("Vertical"); // Vertical Movement Variable
 
-        rb.AddRelativeForce(new Vector3(hi * movementModifier, rb.velocity.y, vi * movementModifier)); // Allows the basic movement of the character, not currently allowing for jump.
+        rb.AddRelativeForce(new Vector3(hi * movementModifier, rb.velocity.y, vi * movementModifier).normalized, ForceMode.Impulse); // Allows the basic movement of the character, not currently allowing for jump.
         if (Input.GetButtonDown("Jump") && isGrounded()) // Checks that the user has pressed jump and that they arje on the ground.
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpModifier, rb.velocity.z);
         }
 
-        float turn = Input.GetAxis("Mouse X");
+        if (rb.position.y <= -50f)
+        {
+            rb.position = new Vector3(0, 1.5f, 0);
+        }
+
+        float turn = Input.GetAxis("Mouse X") * mouseSensitivityX;
         transform.Rotate(new Vector3(0, turn, 0), Space.Self);
+
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = cursorVisibility;
     }
 
     bool isGrounded()
